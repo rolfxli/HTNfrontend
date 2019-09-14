17d147495c85b97b147f70b322776a81d27fb0f5
Rolf Li <rolf.x.li@gmail.com>
2019-09-14 13:20:59 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+        >
+        </a>
+    </div>
+  );
+}
+
+export default App;
